be929839e77ec46e854cc2cb61f599abacb1b697
Eugeneh1217 <Eugeneh1217@gmail.com>
2020-09-18 08:47:51 -0400

    Initialized first version

---
+++ uninstaller.bat
@@ -0,0 +1,33 @@
+@echo off
+
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
+
+:: remove from path
+setx PATH "%PATH:C:\Users\eugen\AppData\Local\Programs\background_image\dist=%"
+
+:: remove files and directories for command
+set install_path=C:\Users\%USERNAME%\AppData\Local\Programs
+rmdir /s /q %install_path%\background_image
+
